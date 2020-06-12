5e2e543b84e5578d05f6b9996a718d6346922a90
echo <echo@gamil.com>
2020-06-12 11:32:46 +0800

    go mmap

---
+++ uinix_mmap/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+
+	"golang.org/x/sys/unix"
+)
+
+const n = 1e3
+
+func main() {
+	f, err := os.OpenFile("./data.txt", os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		panic(err)
+	}
+	length := int(fi.Size())
+	fmt.Printf("length:%d\n", length)
+
+	b, err := unix.Mmap(int(f.Fd()), 0, 1<<20, syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(len(b), cap(b))
+	b[0] = '1'
+	b[length-1] = 'q'
+	b[length] = 'f'
+
+	for i, v := range "i love you forever" {
+		b[length+i] = byte(v)
+	}
+
+	unix.Msync(b, unix.MS_SYNC)
+
+	err = unix.Munmap(b)
+	if err != nil {
+		panic(err)
+	}
+
+	//fmt.Println(string(b))
+}
